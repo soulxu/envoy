b09cb7653ebd99cf4019204c914ef7d380855c8e
He Jie Xu <hejie.xu@intel.com>
2021-09-08 11:03:25 +0000

    fix
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/common/buffer/buffer_impl.h
+++ source/common/buffer/buffer_impl.h
@@ -394,11 +394,9 @@ protected:
 
     if (isCachedStorage(storage.get())) {
       FreeListType& free_list = free_list_opt->free_list_;
-      if (free_list.size() <= free_list_max_) {
         free_list.emplace_back(storage.release());
         ASSERT(storage == nullptr);
         return;
-      }
     }
 
     storage.reset();
@@ -799,7 +797,6 @@ private:
 
     ~OwnedImplReservationSlicesOwnerMultiple() override {
       while (!owned_slices_.empty()) {
-        owned_slices_.back().freeStorage(free_list_);
         owned_slices_.pop_back();
       }
     }
