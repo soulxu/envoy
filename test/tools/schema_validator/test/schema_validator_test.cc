066283cb525c80876f32fad5c626bf70f4f3bf96
He Jie Xu <hejie.xu@intel.com>
2022-05-09 06:42:47 +0000

    ListenerImpl: parse multiple addresses
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- test/tools/schema_validator/test/schema_validator_test.cc
+++ test/tools/schema_validator/test/schema_validator_test.cc
@@ -98,9 +98,8 @@ TEST_F(SchemaValidatorTest, BootstrapPgvFail) {
 TEST_F(SchemaValidatorTest, LdsRecursivePgvFail) {
   EXPECT_THROW_WITH_REGEX(
       run("schema_validator_tool -c {} -t discovery_response", "lds_pgv_fail.yaml"), EnvoyException,
-      "Proto constraint validation failed \\(HttpConnectionManagerValidationError.RouteConfig:"
-      ".* caused by RouteConfigurationValidationError.VirtualHosts.*"
-      "VirtualHostValidationError.Domains: value must contain at least 1 item\\(s\\)");
+      "Proto constraint validation failed \\(ListenerValidationError.FilterChains\\[0\\]: embedded "
+      "message failed validation");
 }
 
 } // namespace Envoy
