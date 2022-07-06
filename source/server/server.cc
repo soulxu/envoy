2f1df287092329c4ef19eaa9b93daa44745137fd
He Jie Xu <hejie.xu@intel.com>
2022-07-06 05:04:57 +0000

    Add dimitry's implement and move to the core
    Signed-off-by: Dmitry Rozhkov <dmitry.rozhkov@intel.com>
    
    Signed-off-by: He Jie Xu <hejie.xu@intel.com>

--- source/server/server.cc
+++ source/server/server.cc
@@ -583,14 +583,25 @@ void InstanceImpl::initialize(Network::Address::InstanceConstSharedPtr local_add
         std::move(safe_actions), std::move(unsafe_actions), api_->threadFactory());
   }
 
+  Network::SocketInterface* sock = nullptr;
+
   if (!bootstrap_.default_socket_interface().empty()) {
     auto& sock_name = bootstrap_.default_socket_interface();
-    auto sock = const_cast<Network::SocketInterface*>(Network::socketInterface(sock_name));
-    if (sock != nullptr) {
-      Network::SocketInterfaceSingleton::clear();
-      Network::SocketInterfaceSingleton::initialize(sock);
-    }
+    sock = const_cast<Network::SocketInterface*>(Network::socketInterface(sock_name));
   }
+
+  if (bootstrap_.default_socket_interface().empty() || sock == nullptr) {
+    auto factory =
+        Registry::FactoryRegistry<Server::Configuration::BootstrapExtensionFactory>::getFactory(
+            "envoy.extensions.network.socket_interface.default_socket_interface");
+    bootstrap_extensions_.push_back(factory->createBootstrapExtension(
+        *factory->createEmptyConfigProto(), serverFactoryContext()));
+    sock = dynamic_cast<Network::SocketInterface*>(factory);
+  }
+
+  ASSERT(sock != nullptr);
+  Network::SocketInterfaceSingleton::clear();
+  Network::SocketInterfaceSingleton::initialize(sock);
 
   // Initialize the regex engine and inject to singleton.
   if (bootstrap_.has_default_regex_engine()) {
